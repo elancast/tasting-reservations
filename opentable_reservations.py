0da9be7442b15a0ddf36f9af940608e8cab6be7d
Emily Lancaster <elancast@Princeton.edu>
2015-03-21 10:35:29 -0400

    use findnextavailable andparse days

--- opentable_reservations.py
+++ opentable_reservations.py
@@ -12,11 +12,28 @@ br = get_browser()
 def get_reservations(dt, restaurant_id, people=2):
   time = urllib.quote_plus(dt.strftime(FORMAT))
   url = URL % (people, time, restaurant_id)
-  print url
   s = open_page(br, url)
-  return _get_times(s)
+  return _get_all_days(s)
 
-# TODO: Days
+def _get_all_days(s):
+  s = s[s.index('id="NextAvailResults_ResultsGrid"'):]
+  days = []
+
+  while True:
+    start = s.find('<td class="ReCol"')
+    if start < 0: break
+    s = s[start:]
+
+    start = s.index('">') + 2
+    end = s.index('<', start)
+
+    date = s[start:end]
+    times = _get_times(s)
+    days.append((date, times))
+    s = s[end:]
+
+  return days
+
 def _get_times(s):
   s = s[s.find('ResultTimes'):]
   s = s[:s.find('</ul')]
@@ -35,12 +52,17 @@ def _get_times(s):
   return times
 
 if __name__ == '__main__':
-  tomorrow = datetime.datetime(2015, 3, 21, 19, 0)
-  res = get_reservations(tomorrow, 211, 2)
+  day = datetime.datetime(2015, 3, 28, 19, 0)
+  res = get_reservations(day, 281, 2)
   print 'got', len(res)
   print str(res)
 
-  tomorrow = datetime.datetime(2015, 3, 21, 19, 0)
-  res = get_reservations(tomorrow, 24433, 2)
+  day = datetime.datetime(2015, 3, 28, 19, 0)
+  res = get_reservations(day, 24433, 2)
+  print 'got', len(res)
+  print str(res)
+
+  day = datetime.datetime(2015, 3, 28, 19, 0)
+  res = get_reservations(day, 820, 2)
   print 'got', len(res)
   print str(res)
